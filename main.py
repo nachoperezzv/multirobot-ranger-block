f57f0c73967f955adf0fbbb4e234ec0aea006dc4
nachoperezzv <ignacioperezzv@gmail.com>
2022-05-11 12:42:51 +0200

    Actualización del main y añadido fichero ranger para movimiento de los ranger. En cada fichero esta escrito lo que tiene que hacer cada uno

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from aurigapy import *
 
-ap = AurigaPy(debug=False)
 
-print("No hay error")
+# crear una interfaz
+
+# cuando pulses botón start publicas en los topics que pueden empezar
+
+# Creas un bucle infinito para que cuando recibas un mensaje de que han llegado
+# a posicion subas uno en el contador 0/3, 1/3, 2/3, 3/3
+
+# Cuando tengas 3/3
+
+# Mandar mensaje de volver a casa a los robots que estén pulalando por ahí todavia
+
+
+
+
+
 
---
+++ ranger.py
@@ -0,0 +1,13 @@
+from aurigapy import *
+
+
+# son lanzados por un fichero launch que les manda por parámetro
+# el nombre de conexión
+
+# Leen de un topic cuando tienen que empezar su movimiento
+
+# Publican en un topic cuando han encontrado el punto de luz
+
+# Se mueven en un bucle infinito que busca la luz. Aquí entra la 
+# máquina de estados. 
+
